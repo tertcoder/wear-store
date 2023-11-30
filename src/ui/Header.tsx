6ed1e6e5739d85772473b77b507835faaea3e59e
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2023-11-30 17:59:05 +0200

    Update Header.tsx

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -4,18 +4,18 @@ import UserImg from "../assets/images/user.png";
 
 function Header() {
   return (
-    <nav className="flex w-full items-center justify-between border">
-      <div className="border-bd-main shadow-shdw-main  bg-btn-gray-bg flex items-center gap-4 rounded-full border p-4">
+    <nav className="flex w-full items-center justify-between">
+      <div className="flex items-center  gap-4 rounded-full border border-bd-main bg-btn-gray-bg p-4 shadow-shdw-main">
         <img src={MenuIcon} alt="Menu icon" />
-        <span className="text-txt-main font-sans text-lg font-semibold">
+        <span className="font-sans text-lg font-semibold text-txt-main">
           Homee
         </span>
       </div>
 
       <img src={Logo} alt="WearStore Logo Icon" className="w-28" />
-      <div className="border-bd-main bg-btn-gray-bg shadow-shdw-main flex items-center gap-3 rounded-full border pr-4">
+      <div className="flex items-center gap-3 rounded-full border border-bd-main bg-btn-gray-bg pr-4 shadow-shdw-main">
         <img src={UserImg} alt="User Name" className="h-14 w-14 rounded-full" />
-        <span className="text-txt-main font-sans text-lg font-semibold">
+        <span className="font-sans text-lg font-semibold text-txt-main">
           tertcoder
         </span>
       </div>
