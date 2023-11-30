cdd11a9c5a19b316a43a70ba1e257ff9c408d83f
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2023-11-30 17:56:29 +0200

    update

--- src/features/home/Hero.tsx
+++ src/features/home/Hero.tsx
@@ -2,8 +2,15 @@ import HeroImg from "../../assets/images/hero.jpg";
 function Hero() {
   return (
     <div className="grid w-full grid-cols-[1fr_585px] rounded-[2.5rem]">
-      <div></div>
-      <img src={HeroImg} alt="Hero Image" />
+      <div>
+        <div>
+          <div>
+            <h1>Start Wearing Beautiful Sneaker</h1>
+          </div>
+          <button></button>
+        </div>
+      </div>
+      <img src={HeroImg} alt="Hero Image" className="object-cover" />
     </div>
   );
 }
