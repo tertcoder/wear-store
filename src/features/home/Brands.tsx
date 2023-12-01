59b6446cff39d4e9a91f2cfbaf2368da212e17a9
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2023-12-01 11:33:19 +0200

    brands components updates

--- src/features/home/Brands.tsx
+++ src/features/home/Brands.tsx
@@ -11,16 +11,43 @@ function Brands() {
         <img src={LineIcon} alt="Line svg" />
       </div>
       <div className="relative flex h-24 w-full flex-col justify-center py-[0.625rem]">
-        <div className="flex  items-center gap-5 ">
+        {/* <div className="flex  items-center gap-5 ">
           {brands.map((brand) => (
             <div
               key={brand.brand}
+              style={{
+                background: `linear-gradient(
+                  0deg, 
+                  rgba(40, 35, 28, 0.30) 0%, 
+                  rgba(40, 35, 28, 0.30) 100%),
+                   url(${brand.image}), 
+                   lightgray 50% / cover no-repeat;`,
+              }}
               className="flex w-56 flex-shrink-0 items-center justify-center gap-2.5 rounded-full border border-bd-gray px-[3.25rem] py-[1.125rem]"
             >
               <span className="text-red text-2xl font-bold">{brand.brand}</span>
             </div>
           ))}
+        </div> */}
+        <div className="flex items-center gap-5">
+          {brands.map((brand) => (
+            <div
+              key={brand.brand}
+              style={{
+                background: `linear-gradient(
+                  0deg, 
+                  rgba(40, 35, 28, 0.20) 0%, rgba(40, 35, 28, 0.20) 100%),
+                    url(${brand.image}) center/cover no-repeat`,
+              }}
+              className="flex w-56 flex-shrink-0 items-center justify-center gap-2.5 rounded-full border border-bd-gray px-[3.25rem] py-[1.125rem]"
+            >
+              <span className="text-2xl font-bold text-white">
+                {brand.brand}
+              </span>
+            </div>
+          ))}
         </div>
+
         <div className="absolute left-0 right-0 top-1/2 mx-4 flex h-full -translate-y-1/2 items-center justify-between">
           <BtnNav icon={PrevIcon} actionType="Previous" />
           <BtnNav icon={NextIcon} actionType="Next" />
